97d010725991865ebb177aae7bc9a190e92e3ac8
Gokac9 <theabraham9@gmail.com>
2024-09-23 11:30:31 +0200

    Update the button to see the assitance from a contact-student

--- seta_groups_and_attendance/models/attendance_record_line.py
+++ seta_groups_and_attendance/models/attendance_record_line.py
@@ -7,3 +7,6 @@ class AttendanceRecordLine(models.Model):
     attendance_record_id = fields.Many2one('attendance.record', string='Attendance Record', ondelete='cascade')
     student_id = fields.Many2one('res.partner', string='Student', domain=[('is_company', '=', False)], required=True)
     present = fields.Boolean(string='Present', default=True)
+
+    activity = fields.Char(related='attendance_record_id.activity', string='Actividad', store=True)
+    group_id = fields.Many2one(related='attendance_record_id.group_id', string='Grupo', store=True)

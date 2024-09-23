2272198998c33fdbee5b969fc791b56f97f69dd5
Gokac9 <theabraham9@gmail.com>
2024-09-23 08:48:30 +0200

    some translates and add button for see assitances from res partner

--- seta_groups_and_attendance/models/__init__.py
+++ seta_groups_and_attendance/models/__init__.py
@@ -1,3 +1,4 @@
 from . import contact_group
 from . import attendance_record
 from . import attendance_record_line
+from . import res_partner
--- seta_groups_and_attendance/models/res_partner.py
+++ seta_groups_and_attendance/models/res_partner.py
@@ -1,19 +1,13 @@
-# Copyright (C) 2024 Manuel Calero (<https://xtendoo.es>).
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-from odoo import models, fields
-
+from odoo import models, fields, api
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    # def action_view_attendance_records(self):
-    #     self.ensure_one()
-    #     return {
-    #         'type': 'ir.actions.act_window',
-    #         'name': 'Attendance Records',
-    #         'view_mode': 'tree,form',
-    #         'res_model': 'attendance.record',
-    #         'domain': [('attendance_record_line_ids.student_id', '=', self.id)],
-    #         'context': dict(self._context),
-    #     }
+    attendance_count = fields.Integer(string='Attendance Count', compute='_compute_attendance_count')
+
+    @api.depends('attendance_count')
+    def _compute_attendance_count(self):
+        AttendanceLine = self.env['attendance.record.line']
+        for partner in self:
+            attendance_lines = AttendanceLine.search([('student_id', '=', partner.id)])
+            partner.attendance_count = len(attendance_lines)
